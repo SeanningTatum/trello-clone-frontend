0e2958fa8cb819e47ee7ef8d69cc3f5c120c17bf
Sean Urgel <seantheurgel@gmail.com>
2019-03-04 21:53:25 +0800

    create navbar

---
+++ components/dashboard/navbar.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {
+  Collapse,
+  Navbar as BSNavbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <BSNavbar dark expand="md" style={{height: '40px', backgroundColor: '#026aa7', color: 'white'}}>
+      <NavbarBrand href="/">Trello Clone</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </BSNavbar>
+  )
+}
---
+++ pages/boards.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container} from 'reactstrap'
+
+import Navbar from '../components/dashboard/navbar'
+
+export default function Boards() {
+  return (
+    <div>
+      <Navbar />
+      <Container>
+        <h5>Hello</h5>
+      </Container>
+    </div>
+  )
+}
