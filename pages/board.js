b5ba839fa8f76f7ec9e4f57e790c961e87479c02
Sean Urgel <seantheurgel@gmail.com>
2019-03-05 16:21:17 +0800

    begin restart

--- pages/board.js
+++ pages/board.js
@@ -3,39 +3,37 @@
 import React, {useState} from 'react'
 import {Container} from 'reactstrap'
 import {type DropResult} from 'react-beautiful-dnd'
+import styled from 'styled-components'
 
 import {type BoardItem} from '../interfaces/BoardItem'
 import Navbar from '../components/dashboard/Navbar'
 import NavTitleAndActions from '../components/board/NavTitleAndActions'
-import BoardList from '../components/board/List'
 
-const testItems: Array<BoardItem> = [
-  {id: 1, content: '1'},
-  {id: 2, content: '2'},
-  {id: 3, content: '3'},
-]
+const sampleBoardData = {
+  tasks: {
+    'task-1': {id: 'task-1', content: 'Take out the garbage'},
+    'task-2': {id: 'task-2', content: 'Take out the garbage'},
+    'task-3': {id: 'task-3', content: 'Take out the garbage'},
+    'task-4': {id: 'task-4', content: 'Take out the garbage'},
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'Todo',
+      tasksIds: ['task-1', 'task-2'],
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'Another todo',
+      tasksIds: ['task-3', 'task-4'],
+    },
+  },
+
+  columnOrder: ['column-1'],
+}
 
 export default function Board() {
-  const [items: Array<BoardItem>, setItems] = useState(testItems)
-
-  // Utility function to reorder
-  function reorder(array, startIndex, endIndex): Array<{id: number, content: string}> {
-    const newArray = [...array]
-    const [removed] = newArray.splice(startIndex, 1)
-    newArray.splice(endIndex, 0, removed)
-
-    return newArray
-  }
-
-  function onDragEnd(result: DropResult): void {
-    // dropped outside the list
-    if (!result.destination) return
-
-    // Reorder the items
-    const newItems = reorder(items, result.source.index, result.destination.index)
-
-    setItems(newItems)
-  }
+  const [board, setBoard] = useState(sampleBoardData)
 
   return (
     <div>
@@ -43,8 +41,21 @@ export default function Board() {
       <NavTitleAndActions />
       <Container fluid>
         <h5>Things</h5>
-        <BoardList onDragEnd={onDragEnd} items={items} />
+        <BoardList />
       </Container>
     </div>
   )
 }
+
+const ColumnContainer = styled.div`
+  width: 250px;
+  padding: 8px;
+  background-color: ${props => (props.isDraggingOver ? 'lightblue' : 'lightgrey')};
+`
+
+const StyledListItem = styled.div`
+  userselect: 'none';
+  padding: 16px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'grey')};
+`
