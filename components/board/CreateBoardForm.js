2a91fef19e8a2f2e0d06f43ae0fd18c4839cbf69
Sean Urgel <seantheurgel@gmail.com>
2019-03-08 13:33:26 +0800

    Add form input to createboard form

--- components/board/CreateBoardForm.js
+++ components/board/CreateBoardForm.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-import {Row, Col, Button} from 'reactstrap'
+import {Row, Col, Button, Input} from 'reactstrap'
+
+import useFormInput from '../../hooks/useFormInput'
 
 const COLORS = [
   'rgb(0, 121, 191)',
@@ -15,26 +17,43 @@ const COLORS = [
 ]
 
 export default function CreateBoardForm() {
+  const [currentBg, setCurrentBg] = useState(COLORS[0])
+  const {setValue: setBoardName, ...boardName} = useFormInput('')
+
+  function changeCurrentBg(newColor) {
+    setCurrentBg(newColor)
+  }
+
   return (
     <FormContainer>
-      <Row className="mb-1">
-        <Col md={8}>
-          <CreateBoardTile>
-            <p>Add board title</p>
+      <Row className="mb-1" noGutters>
+        <Col md={7} className="mr-3">
+          <CreateBoardTile style={{backgroundColor: currentBg}}>
+            <CreateFormInput placeholder="Add board title" {...boardName} />
           </CreateBoardTile>
         </Col>
-        <Col md={4}>
+        <Col md={3}>
           <Row noGutters>
             {COLORS.map((color, ndx) => (
               <Col xs={4}>
-                <Tile style={{backgroundColor: color}}>{ndx + 1}</Tile>
+                <Tile style={{backgroundColor: color}} onClick={() => changeCurrentBg(color)}>
+                  {ndx + 1}
+                </Tile>
               </Col>
             ))}
           </Row>
         </Col>
       </Row>
 
-      <Button color="success">
+      <Button
+        color="success"
+        type="button"
+        style={{
+          backgroundColor: boardName.value === '' && 'rgba(9, 45, 66, 1)',
+          borderColor: boardName.value === '' && 'rgba(9, 45, 66, 1)',
+        }}
+        disabled={boardName.value === ''}
+      >
         <span>Create Board</span>
       </Button>
     </FormContainer>
@@ -44,6 +63,26 @@ export default function CreateBoardForm() {
 const FormContainer = styled.form`
   margin: 0 auto;
   width: fit-content;
+`
+
+const CreateFormInput = styled(Input)`
+  background: transparent;
+  border: none;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  color: white;
+
+  &::placeholder {
+    color: white !important;
+  }
+
+  &:focus {
+    background-color: rgba(255, 255, 255);
+    opacity: 0.5;
+    color: black;
+  }
 `
 
 const CreateBoardTile = styled.div`
