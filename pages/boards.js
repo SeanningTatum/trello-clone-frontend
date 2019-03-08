a61636d32baf7bdd041a07ae0d8517307c13b74f
Sean Urgel <seantheurgel@gmail.com>
2019-03-08 12:32:20 +0800

    create basic styled modal

--- pages/boards.js
+++ pages/boards.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-import {Container, Row, Col} from 'reactstrap'
+import {Container, Row, Col, Modal} from 'reactstrap'
 
 import Navbar from '../components/dashboard/Navbar'
 import Sidenav from '../components/dashboard/Sidenav'
 import BoardList from '../components/dashboard/BoardList'
 
 export default function Boards() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  function onCreateNewBoardClicked() {
+    setIsModalOpen(true)
+  }
+
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen)
+  }
+
   return (
     <div>
       <Navbar />
@@ -20,14 +30,30 @@ export default function Boards() {
             <Row className="justify-space-between">
               <BoardList boards={[1, 2, 3, 4, 5]} />
               <Col md={3}>
-                <CreateBoardBox className="py-3 ">
+                <CreateBoardBox className="py-3" onClick={onCreateNewBoardClicked}>
                   <p className="mb-0">Create new board...</p>
                 </CreateBoardBox>
               </Col>
             </Row>
           </Col>
         </Row>
+
+        <StyledModal
+          isOpen={isModalOpen}
+          toggle={toggleModal}
+          fade={false}
+          contentClassName="modalContent"
+        >
+          <h5>Hello</h5>
+        </StyledModal>
       </Container>
+
+      {/* Use global because modal is defined outside of the component */}
+      <style jsx global>{`
+        .modalContent {
+          background-color: transparent;
+        }
+      `}</style>
     </div>
   )
 }
@@ -41,4 +67,9 @@ const CreateBoardBox = styled.div`
   colors: #6b808c;
   background-color: rgba(9, 45, 66, 0.08);
   cursor: pointer;
+`
+
+const StyledModal = styled(Modal)`
+  margin-top: 3.5rem;
+  background-color: transparent;
 `
